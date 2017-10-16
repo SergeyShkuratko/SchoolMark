8bf56776fde37caa85a94a00ba533977391ebe45
vshakirov <v.shakirov@innopolis.ru>
2017-10-16 20:49:58 +0300

    промежуточные изменения

--- core/src/main/java/classes/Teacher.java
+++ core/src/main/java/classes/Teacher.java
@@ -1,12 +1,12 @@
 package classes;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class Teacher {
+public class Teacher extends User {
     private final int id;
-    private final int userId;
     private final String lastname;
     private final String firstname;
     private final String patronymic;
@@ -17,9 +17,9 @@ public class Teacher {
     private final String qualification;
 
     //TODO вернуть тип subject перед комитом
-    public Teacher(int id, int userId, String lastname, String firstname, String patronymic, School school, Collection<Subject> subject, int minClass, int maxClass, String qualification) {
+    public Teacher(int id, int userId, String login, LocalDate regDate, String lastname, String firstname, String patronymic, School school, Collection<Subject> subject, int minClass, int maxClass, String qualification) {
+        super(userId, login, regDate);
         this.id = id;
-        this.userId = userId;
         this.lastname = lastname;
         this.firstname = firstname;
         this.patronymic = patronymic;
@@ -33,10 +33,6 @@ public class Teacher {
 
     public int getId() {
         return id;
-    }
-
-    public int getUserId() {
-        return userId;
     }
 
     public String getLastname() {
