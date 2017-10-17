50599fcddb8ba64bfad6b322622cf832444c54d2
ejenfransuavidok <ejenfransuavidok@gmail.com>
2017-10-17 19:05:38 +0300

    created some dao for Student, School, SchoolClass

--- student/src/main/java/dao/DAOSchoolClassImpl.java
+++ student/src/main/java/dao/DAOSchoolClassImpl.java
@@ -1,19 +1,76 @@
 package dao;
 
 import classes.SchoolClass;
+import classes.Student;
+import connectionmanager.ConnectionManagerPostgresImpl;
 import exception.DAOSchoolClassErrorRequestException;
+import exception.DAOSchoolErrorRequestException;
+import exception.DAOStudentErrorRequestException;
+import org.apache.log4j.Logger;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class DAOSchoolClassImpl implements DAOSchoolClass {
-    @Override
-    public SchoolClass getSchoolClassById(int id) throws DAOSchoolClassErrorRequestException {
-        return null;
+
+    private final Logger logger = Logger.getLogger(DAOStudent.class);
+    private final ConnectionManagerPostgresImpl connectionManagerPostgres
+            = ConnectionManagerPostgresImpl.getInstance();
+    private final Connection connection;
+    private final String baseGetSql = "" +
+            "SELECT * FROM school_classes schl_cls ";
+    private final String baseRemoveSql = "DELETE FROM school_classes ";
+    private final String insertSql = "INSERT INTO school_classes " +
+            "(id, number, name, school_id) VALUES " +
+            "(?, ?, ?, ?)";
+    private final String updateSql = "UPDATE school_classes " +
+            "SET number = ?, name = ?, school_id = ? " +
+            "WHERE id = ?";
+
+    public DAOSchoolClassImpl() throws SQLException, DAOSchoolClassErrorRequestException {
+        if(connectionManagerPostgres == null)
+            throw new DAOSchoolClassErrorRequestException("public DAOSchoolClassImpl(): connectionManagerPostgres is null !!!");
+        connection = connectionManagerPostgres.getConnection();
+    }
+
+    private SchoolClass getOne(String where) throws DAOSchoolClassErrorRequestException {
+        try (ResultSet rs = DAOUtils.getResultSetExecuteQueryByWhere(
+                connection, baseGetSql + where)) {
+            if (rs.next()) {
+                return DAOUtils.getSchoolClassByResultSet(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DAOSchoolClassErrorRequestException(e.getMessage());
+        }
+    }
+
+    private ArrayList<SchoolClass> getMany(String where)
+            throws DAOSchoolClassErrorRequestException {
+        try (ResultSet rs = DAOUtils.getResultSetExecuteQueryByWhere(
+                connection, baseGetSql + where)) {
+            ArrayList<SchoolClass> schoolClasses = new ArrayList<>();
+            while (rs.next()) {
+                schoolClasses.add(DAOUtils.getSchoolClassByResultSet(rs));
+            }
+            return schoolClasses.size() != 0 ? schoolClasses : null;
+        } catch (SQLException e) {
+            throw new DAOSchoolClassErrorRequestException(e.getMessage());
+        }
     }
 
     @Override
-    public ArrayList<SchoolClass> getSchoolClassesByNumber(int number) throws DAOSchoolClassErrorRequestException {
-        return null;
+    public SchoolClass getSchoolClassById(int id)
+            throws DAOSchoolClassErrorRequestException {
+        return this.getOne("WHERE schl_cls.id = " + id);
+    }
+
+    @Override
+    public ArrayList<SchoolClass> getSchoolClassesByNumber(int number)
+            throws DAOSchoolClassErrorRequestException {
+        return this.getMany("WHERE schl_cls.number = " + number);
     }
 
     @Override
