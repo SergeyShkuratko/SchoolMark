b556a7f2fae3344d1dd3c5d59b9494779f2a49cd
Artyom Zolotoverkhov <artyomzolotoverkhov@gmail.com>
2017-10-24 22:10:24 +0300

    Изменил  SQL на константы

--- organizer/src/main/java/inno/dao/TestDAO.java
+++ organizer/src/main/java/inno/dao/TestDAO.java
@@ -5,6 +5,7 @@ import connectionmanager.TomcatConnectionPool;
 import inno.exceptions.OrganizerDAOexception;
 import org.apache.log4j.Logger;
 import org.postgresql.util.PGobject;
+
 import java.sql.*;
 import java.time.LocalDate;
 
@@ -16,7 +17,7 @@ public class TestDAO {
 
     public static boolean updateTestStatusByID(int id, String status) throws OrganizerDAOexception {
         System.out.println("go");
-        String sql = "UPDATE tests SET status = ? WHERE id= ?";
+        final String sql = "UPDATE tests SET status = ? WHERE id= ?";
         try (Connection connection = pool.getConnection();
              PreparedStatement ps = connection.prepareStatement(sql)) {
 
@@ -34,8 +35,7 @@ public class TestDAO {
 
 
     public static boolean doneTest(int id) throws OrganizerDAOexception {
-        System.out.println("go");
-        String sql = "UPDATE tests SET status = ?, verification_deadline = ? WHERE id= ?";
+       final String sql = "UPDATE tests SET status = ?, verification_deadline = ? WHERE id= ?";
         try (Connection connection = pool.getConnection();
              PreparedStatement ps = connection.prepareStatement(sql)) {
 
