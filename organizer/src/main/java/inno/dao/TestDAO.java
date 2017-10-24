c788b7a0ba3cea04ae25e5e7b908e3981b0dac6d
Artyom Zolotoverkhov <artyomzolotoverkhov@gmail.com>
2017-10-24 22:47:15 +0300

    Изменил  SQL на константы, дописал javaDoc

--- organizer/src/main/java/inno/dao/TestDAO.java
+++ organizer/src/main/java/inno/dao/TestDAO.java
@@ -15,6 +15,14 @@ public class TestDAO {
 
     private static ConnectionPool pool = TomcatConnectionPool.getInstance();
 
+    /**
+     * Обновляет статус контрольной работы
+     *
+     * @param id     - id контрольной в таблице tests
+     * @param status - статус из перечисления test_status
+     * @return true если обновилось, false если нет
+     * @throws OrganizerDAOexception
+     */
     public static boolean updateTestStatusByID(int id, String status) throws OrganizerDAOexception {
         System.out.println("go");
         final String sql = "UPDATE tests SET status = ? WHERE id= ?";
@@ -24,7 +32,7 @@ public class TestDAO {
             ps.setInt(2, id);
             PGobject status_var = new PGobject();
             status_var.setType("test_status");
-            status_var.setValue("uploaded");
+            status_var.setValue(status);
             ps.setObject(1, status_var);
             return ps.executeUpdate() > 0;
         } catch (SQLException e) {
@@ -33,21 +41,32 @@ public class TestDAO {
         }
     }
 
-
+    /**
+     * Устанавливает тест как завершенный
+     *
+     * @param id
+     * @return
+     * @throws OrganizerDAOexception
+     */
     public static boolean doneTest(int id) throws OrganizerDAOexception {
-       final String sql = "UPDATE tests SET status = ?, verification_deadline = ? WHERE id= ?";
+        final String sql = "UPDATE tests SET status = ?,  WHERE id= ?";
+
         try (Connection connection = pool.getConnection();
              PreparedStatement ps = connection.prepareStatement(sql)) {
 
             ps.setInt(3, id);
+
             PGobject status_var = new PGobject();
             status_var.setType("test_status");
             status_var.setValue("uploaded");
 
+            //Устанавливаем дэдлайн проверки - текущая дата + неделя
             ps.setDate(2, java.sql.Date.valueOf(LocalDate.now().plusDays(7)));
 
             ps.setObject(1, status_var);
+
             return ps.executeUpdate() > 0;
+
         } catch (SQLException e) {
             logger.error(e);
             throw new OrganizerDAOexception(e);
