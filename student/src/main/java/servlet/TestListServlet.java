daf13acd23588758c0eddc213e58d857964f352e
ejenfransuavidok <ejenfransuavidok@gmail.com>
2017-10-27 00:54:45 +0300

    =Коммит на изменение / коррекция

--- student/src/main/java/servlet/TestListServlet.java
+++ student/src/main/java/servlet/TestListServlet.java
@@ -22,20 +22,20 @@ import java.util.List;
 public class TestListServlet extends HttpServlet{
     private final Logger logger = Logger.getLogger(this.getClass());
 
-    @Override
     @RequestMapping(value = "/testlist", method = RequestMethod.GET)
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int user_id = (Integer)req.getSession().getAttribute(CommonSettings.AUTH_USER_ATTRIBUTE);
-        //TODO добавить проверку наличия контрольных на статусе Новый
-        boolean haveNewTest = true;
-        req.setAttribute("have_new_test", haveNewTest);
-        List<DTOWork> works = null;
-        try {
-            works = WorkService.getAllWork(user_id);
-            req.setAttribute("works", works);
-        } catch (DAOStudentWork.DAOStudentWorkException e) {
-            e.printStackTrace();
-        }
-        req.getRequestDispatcher("/testlist.jsp").forward(req, resp);
-    }
+
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        int user_id = (Integer)req.getSession().getAttribute(CommonSettings.AUTH_USER_ATTRIBUTE);
+//        //TODO добавить проверку наличия контрольных на статусе Новый
+//        boolean haveNewTest = true;
+//        req.setAttribute("have_new_test", haveNewTest);
+//        List<DTOWork> works = null;
+//        try {
+//            works = WorkService.getAllWork(user_id);
+//            req.setAttribute("works", works);
+//        } catch (DAOStudentWork.DAOStudentWorkException e) {
+//            e.printStackTrace();
+//        }
+//        req.getRequestDispatcher("/testlist.jsp").forward(req, resp);
+//    }
 }
