285d82747356dd353347dec38ce114a4db09e6df
Maxim Myshkin <makc994@gmail.com>
2017-10-26 17:31:41 +0300

    Inject dependencies to SchoolServlet

--- auth/src/main/java/servlets/admin/SchoolServlet.java
+++ auth/src/main/java/servlets/admin/SchoolServlet.java
@@ -1,26 +1,40 @@
 package servlets.admin;
 
-import classes.City;
-import classes.SchoolType;
 import classes.dto.CityDTO;
 import classes.dto.SchoolDTO;
 import exceptions.CityDAOException;
 import exceptions.SchoolDAOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 import services.CityService;
 import services.SchoolService;
-import services.impl.CityServiceImpl;
-import services.impl.SchoolServiceImpl;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public class SchoolServlet extends HttpServlet {
-    CityService cityService = new CityServiceImpl();
-    SchoolService schoolService = new SchoolServiceImpl();
+    private CityService cityService;
+    private SchoolService schoolService;
+
+    @Autowired
+    public void setCityService(CityService cityService) {
+        this.cityService = cityService;
+    }
+
+    @Autowired
+    public void setSchoolService(SchoolService schoolService) {
+        this.schoolService = schoolService;
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
