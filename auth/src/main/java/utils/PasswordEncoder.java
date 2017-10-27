c6334c1ab20ac6510101088b703dc74f4b56c95e
Crazy1305 <vnshakirov@gmail.com>
2017-10-27 08:10:22 +0300

    Revert "Добавил encoding параметр для строк"
    This reverts commit 8c46eedcda68a63125eaa0cec8e33249a4141cae.

--- auth/src/main/java/utils/PasswordEncoder.java
+++ auth/src/main/java/utils/PasswordEncoder.java
@@ -1,8 +1,5 @@
 package utils;
 
-import org.apache.log4j.Logger;
-
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -10,19 +7,16 @@ import static utils.Settings.SALT;
 
 public class PasswordEncoder {
 
-    private static Logger logger = Logger.getLogger(PasswordEncoder.class);
-
     private static String md5(String password) {
         String result = null;
         try {
-            byte[] passwordBytes = password.getBytes("UTF-8");
+            byte[] passwordBytes = password.getBytes();
             byte[] encodedPassword;
             MessageDigest md = MessageDigest.getInstance("MD5");
             encodedPassword = md.digest(passwordBytes);
-            result = new String(encodedPassword, "UTF-8");
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-            logger.error(e.getMessage(), e);
-            // TODO пробрасывать исключение дальше
+            result = new String(encodedPassword);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
         return result;
     }
