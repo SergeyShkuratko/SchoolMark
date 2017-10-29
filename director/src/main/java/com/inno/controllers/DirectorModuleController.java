6ca71afda197c335609a77060b609ad58dee4da4
Demon81 <Programm81>
2017-10-29 13:05:12 +0300

    Методы конвертации даты вынесены в отдельный класс временно используется фейковый ID пользователя, будет переделано после реализации Spring Security

--- director/src/main/java/com/inno/controllers/DirectorModuleController.java
+++ director/src/main/java/com/inno/controllers/DirectorModuleController.java
@@ -1,7 +1,7 @@
 package com.inno.controllers;
 
 import com.inno.service.TestStatisticService;
-import com.inno.service.TestStatisticServiceImpl;
+import com.inno.utils.DateConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +10,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+import java.util.Calendar;
+
 
 @Controller
 public class DirectorModuleController {
 
     private TestStatisticService testStatisticService;
+    private DateConverter dc;
+    private int userId = 11;
+
+    @Autowired
+    public void setDc(DateConverter dc) {
+        this.dc = dc;
+    }
 
     @Autowired
     public void setTestStatisticService(TestStatisticService testStatisticService) {
@@ -24,38 +31,38 @@ public class DirectorModuleController {
     }
 
     @RequestMapping(value = "/director-test-list", method = RequestMethod.GET)
-    public ModelAndView getTestListView(@RequestParam(name = "groupByOrganization", required=false) String groupByOrganization,
-                                        @RequestParam(name = "dateFrom", required=false) String dateFromStr,
-                                        @RequestParam(name = "dateTo", required=false) String dateToStr)
-    {
-        LocalDate dateFrom = parseDate(dateFromStr);
-        LocalDate dateTo = parseDate(dateToStr);
+    public ModelAndView getTestListView(@RequestParam(name = "groupByOrganization", required = false) String groupByOrganization,
+                                        @RequestParam(name = "dateFrom", required = false) String dateFromStr,
+                                        @RequestParam(name = "dateTo", required = false) String dateToStr) {
+
+        if (dateFromStr == null) {
+            dateFromStr = "01.01."+Calendar.getInstance().get(Calendar.YEAR);
+        }
+        LocalDate dateFrom = dc.parseStringToLocalDate(dateFromStr);
+
+        LocalDate dateTo;
+        if (dateToStr == null) {
+            dateTo = LocalDate.now();
+        } else {
+            dateTo = dc.parseStringToLocalDate(dateToStr);
+        }
+
         ModelAndView modelAndView = new ModelAndView("director-test-list");
+        modelAndView.addObject("dateFrom", dc.formatLocalDateToString(dateFrom));
+        modelAndView.addObject("dateTo", dc.formatLocalDateToString(dateTo));
+
         if (!"on".equals(groupByOrganization)) {
-            modelAndView.addObject("tests", testStatisticService.getTestsStatistic(dateFrom, dateTo));
+            modelAndView.addObject("tests",
+                    testStatisticService.getTestsStatistic(userId, dateFrom, dateTo));
         } else {
             modelAndView.addObject("teacherTestsMap",
-                    testStatisticService.getTestsStatisticGroupedByOwner(dateFrom, dateTo));
+                    testStatisticService.getTestsStatisticGroupedByOwner(userId, dateFrom, dateTo));
         }
         return modelAndView;
     }
 
-    private LocalDate parseDate(String dateStr) {
-        if (dateStr == null) {
-            return null;
-        }
-
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-
-        try {
-            return LocalDate.parse(dateStr, dateTimeFormatter);
-        } catch (DateTimeParseException e) {
-            return null;
-        }
-    }
-
     @RequestMapping(value = "/director-test-view", method = RequestMethod.GET)
-    public ModelAndView getTestView(@RequestParam("testId") int testId){
+    public ModelAndView getTestView(@RequestParam("testId") int testId) {
         return new ModelAndView("director-test-view")
                 .addObject("testAndWorkInfo", testStatisticService.getTestAndWorksInfo(testId));
     }
