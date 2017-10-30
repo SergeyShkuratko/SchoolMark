d0d69d8f08101327c817c937959f4d59eab34ee6
ejenfransuavidok <ejenfransuavidok@gmail.com>
2017-10-30 21:00:51 +0300

    Пофиксил сервлеты / контроллеры

--- student/src/main/java/studentmodule/controllers/WorkLoadServlet.java
+++ student/src/main/java/studentmodule/controllers/WorkLoadServlet.java
@@ -1,10 +1,8 @@
-package studentmodule.servlet;
+package studentmodule.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestParam;
-import studentmodule.exception.DAOStudentWorkException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
